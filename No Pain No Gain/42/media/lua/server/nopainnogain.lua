3d8582ea0e8b42291341b74108ad35dfe4fba2ba
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2025-09-20 09:38:21 -0300

    updated muscle strain into a variable and reduced values

--- No Pain No Gain/42/media/lua/server/nopainnogain.lua
+++ No Pain No Gain/42/media/lua/server/nopainnogain.lua
@@ -1,6 +1,12 @@
 local debug = getSandboxOptions():getOptionByName(
     "NoPainNoGain.ConsoleDebug"):getValue();
 
+local muscleStrainMAX = 100;
+local muscleStrainHIGH = 75;
+local muscleStrainMEDIUM = 50;
+local muscleStrainLOW = 25;
+local muscleStrainMINIMUM = 5;
+
 function xpUpdate.randXp()
     return ZombRand(70 * GameTime.getInstance():getInvMultiplier()) == 0;
 end
@@ -23,7 +29,7 @@ local function onPlayerMove(player)
     if player:IsRunning() or player:isSprinting() then
         if player:IsRunning() then
             if enduranceLevel < 0.25 then
-                player:addRightLegMuscleStrain(2000);
+                player:addRightLegMuscleStrain(muscleStrainMAX);
                 if xpUpdate.randXp() then
                     if not isSleepy(player) then addXp(player, Perks.Fitness, 4); end
                     if debug then
@@ -31,7 +37,7 @@ local function onPlayerMove(player)
                     end
                 end
             elseif enduranceLevel < 0.5 then
-                player:addRightLegMuscleStrain(1000);
+                player:addRightLegMuscleStrain(muscleStrainHIGH);
                 if xpUpdate.randXp() then
                     if not isSleepy(player) then addXp(player, Perks.Fitness, 4); end
                     if debug then
@@ -39,7 +45,7 @@ local function onPlayerMove(player)
                     end
                 end
             elseif enduranceLevel < 0.75 then
-                player:addRightLegMuscleStrain(500);
+                player:addRightLegMuscleStrain(muscleStrainMEDIUM);
                 if xpUpdate.randXp() then
                     if not isSleepy(player) then addXp(player, Perks.Fitness, 3); end
                     if debug then
@@ -56,7 +62,7 @@ local function onPlayerMove(player)
             end
         else
             if enduranceLevel < 0.25 then
-                player:addRightLegMuscleStrain(1000);
+                player:addRightLegMuscleStrain(muscleStrainHIGH);
                 if xpUpdate.randXp() then
                     if not isSleepy(player) then addXp(player, Perks.Fitness, 3); end
                     if debug then
@@ -64,7 +70,7 @@ local function onPlayerMove(player)
                     end
                 end
             elseif enduranceLevel < 0.5 then
-                player:addRightLegMuscleStrain(500);
+                player:addRightLegMuscleStrain(muscleStrainMEDIUM);
                 if xpUpdate.randXp() then
                     if not isSleepy(player) then addXp(player, Perks.Fitness, 2); end
                     if debug then
@@ -83,7 +89,7 @@ local function onPlayerMove(player)
 
         if player:getInventoryWeight() > player:getMaxWeight() * 0.5 then
             if enduranceLevel < 0.25 then
-                player:addBackMuscleStrain(2000);
+                player:addBackMuscleStrain(muscleStrainMAX);
                 if xpUpdate.randXp() then
                     if not isSleepy(player) then addXp(player, Perks.Strength, 3); end
                     if debug then
@@ -91,7 +97,7 @@ local function onPlayerMove(player)
                     end
                 end
             elseif enduranceLevel < 0.5 then
-                player:addBackMuscleStrain(1000);
+                player:addBackMuscleStrain(muscleStrainHIGH);
                 if xpUpdate.randXp() then
                     if not isSleepy(player) then addXp(player, Perks.Strength, 2); end
                     if debug then
@@ -99,7 +105,7 @@ local function onPlayerMove(player)
                     end
                 end
             elseif enduranceLevel < 0.75 then
-                player:addBackMuscleStrain(500);
+                player:addBackMuscleStrain(muscleStrainMEDIUM);
                 if xpUpdate.randXp() then
                     if not isSleepy(player) then addXp(player, Perks.Strength, 1); end
                     if debug then
@@ -110,7 +116,7 @@ local function onPlayerMove(player)
         end
     else
         if enduranceLevel < 0.25 then
-            player:addRightLegMuscleStrain(500);
+            player:addRightLegMuscleStrain(muscleStrainMEDIUM);
             if xpUpdate.randXp() then
                 if not isSleepy(player) then addXp(player, Perks.Fitness, 2); end
                 if debug then
@@ -118,7 +124,7 @@ local function onPlayerMove(player)
                 end
             end
         elseif enduranceLevel < 0.5 then
-            player:addRightLegMuscleStrain(250);
+            player:addRightLegMuscleStrain(muscleStrainLOW);
             if xpUpdate.randXp() then
                 if not isSleepy(player) then addXp(player, Perks.Fitness, 2); end
                 if debug then
@@ -143,19 +149,19 @@ local function onPlayerHitTree(player, weapon)
 
     if weapon and weapon:getType() ~= "BareHands" then
         if enduranceLevel < 0.25 then
-            player:addCombatMuscleStrain(weapon, 1, 1000);
+            player:addCombatMuscleStrain(weapon, 1, muscleStrainHIGH);
             if not isSleepy(player) then addXp(player, Perks.Strength, 12); end
             if debug then
                 DebugPrintNoPainNoGain("Tree hit strength: 5, strain: 1000");
             end
         elseif enduranceLevel < 0.5 then
-            player:addCombatMuscleStrain(weapon, 1, 500);
+            player:addCombatMuscleStrain(weapon, 1, muscleStrainMEDIUM);
             if not isSleepy(player) then addXp(player, Perks.Strength, 6); end
             if debug then
                 DebugPrintNoPainNoGain("Tree hit strength: 2, strain: 500");
             end
         elseif enduranceLevel < 0.75 then
-            player:addCombatMuscleStrain(weapon, 1, 250);
+            player:addCombatMuscleStrain(weapon, 1, muscleStrainLOW);
             if not isSleepy(player) then addXp(player, Perks.Strength, 3) end
             if debug then
                 DebugPrintNoPainNoGain("Tree hit strength: 1, strain: 250");
@@ -174,19 +180,19 @@ local function onPlayerHit(player, weapon, hitObject, damage, hitCount)
     -- Fitness give
     if not weapon:isRanged() then
         if enduranceLevel < 0.25 then
-            player:addCombatMuscleStrain(weapon, 1, 1000);
+            player:addCombatMuscleStrain(weapon, 1, muscleStrainHIGH);
             if not isSleepy(player) then addXp(player, Perks.Fitness, 3); end
             if debug then
                 DebugPrintNoPainNoGain("Hit fitness: 3, strain: 1000");
             end
         elseif enduranceLevel < 0.5 then
-            player:addCombatMuscleStrain(weapon, 1, 500);
+            player:addCombatMuscleStrain(weapon, 1, muscleStrainMEDIUM);
             if not isSleepy(player) then addXp(player, Perks.Fitness, 2); end
             if debug then
                 DebugPrintNoPainNoGain("Hit fitness: 2, strain: 500");
             end
         elseif enduranceLevel < 0.75 then
-            player:addCombatMuscleStrain(weapon, 1, 250);
+            player:addCombatMuscleStrain(weapon, 1, muscleStrainLOW);
             if not isSleepy(player) then addXp(player, Perks.Fitness, 1); end
             if debug then
                 DebugPrintNoPainNoGain("Hit fitness: 1, strain: 250");
@@ -197,19 +203,19 @@ local function onPlayerHit(player, weapon, hitObject, damage, hitCount)
     -- Strength give
     if not weapon:isRanged() and player:getLastHitCount() > 0 then
         if enduranceLevel < 0.25 then
-            player:addCombatMuscleStrain(weapon, 1, 1000 * player:getLastHitCount());
+            player:addCombatMuscleStrain(weapon, 1, muscleStrainHIGH * player:getLastHitCount());
             if not isSleepy(player) then addXp(player, Perks.Strength, player:getLastHitCount() * 10); end
             if debug then
                 DebugPrintNoPainNoGain("Hit strength: 3, strain: " .. 1000 * player:getLastHitCount());
             end
         elseif enduranceLevel < 0.5 then
-            player:addCombatMuscleStrain(weapon, 1, 500 * player:getLastHitCount());
+            player:addCombatMuscleStrain(weapon, 1, muscleStrainMEDIUM * player:getLastHitCount());
             if not isSleepy(player) then addXp(player, Perks.Strength, player:getLastHitCount() * 5); end
             if debug then
                 DebugPrintNoPainNoGain("Hit strength: 2, strain: " .. 500 * player:getLastHitCount());
             end
         elseif enduranceLevel < 0.75 then
-            player:addCombatMuscleStrain(weapon, 1, 250 * player:getLastHitCount());
+            player:addCombatMuscleStrain(weapon, 1, muscleStrainLOW * player:getLastHitCount());
             if not isSleepy(player) then addXp(player, Perks.Strength, player:getLastHitCount() * 3); end
             if debug then
                 DebugPrintNoPainNoGain("Hit strength: 1, strain: " .. 250 * player:getLastHitCount());
